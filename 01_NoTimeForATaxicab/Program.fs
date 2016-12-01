136485fa0b2d8c745acbda5c227475c76141abe1
Chris Jansson <chris.janssom@gmail.com>
2016-12-01 19:31:29 +0100

    Solve the second dialy puzzle

--- 01_NoTimeForATaxicab/Program.fs
+++ 01_NoTimeForATaxicab/Program.fs
@@ -26,6 +26,47 @@ let trim (s:string) =
 let split c (s:string) =
     s.Split([| c |])
 
+type PathingState = 
+    {
+        FirstDoubleLocation : (int * int) option
+        Locations : (int * int) list
+        State : ((int * int) * (int * int))
+    }
+let interpolate (x1, y1) (x2, y2) =
+    let interpolateComponent p0 p1 =
+        if p1 > p0 then
+            [p0..p1]
+        else
+            [p1..p0] |> List.rev
+
+    if x1 <> x2 then
+        interpolateComponent x1 x2
+            |> List.map (fun x -> (x, y1))
+    else
+        interpolateComponent y1 y2
+            |> List.map (fun y -> (x1, y))
+
+let pathingReducer s m =
+        let reduceLocal (locations, doubleVisit) newLocation =
+            let firstDoubleVisit = 
+                    match doubleVisit with
+                    | Some l ->
+                        Some l
+                    | None -> 
+                        if List.contains newLocation locations then
+                            Some newLocation
+                        else
+                            None
+            (locations @ [ newLocation ], firstDoubleVisit)
+    
+        let newDir, newLoc = reduce s.State m
+        let _, currentLocation = s.State
+        let newLocations = interpolate currentLocation newLoc |> List.skip 1
+        
+        let ls, dl = newLocations |> List.fold reduceLocal (s.Locations, s.FirstDoubleLocation)
+        { FirstDoubleLocation = dl; Locations = ls; State = (newDir, newLoc) }
+
+
 [<EntryPoint>]
 let main argv = 
 
@@ -40,11 +81,18 @@ let main argv =
         | _ -> failwith "Cannot parse movement"
 
     let startPosition = (0,1), (0,0)
+    let initialState = {
+            FirstDoubleLocation = None
+            Locations = [ snd startPosition ]
+            State = startPosition
+        }
 
     let result = argv.[0]
                     |> split ','
                     |> Array.map (trim >> parseMovement)
-                    |> Array.fold reduce startPosition
-                    |> snd
-                    |> calculateManhattanDistance
+                    |> Array.fold pathingReducer initialState
+
+    printfn "Distance: %A" (result.State |> snd |> calculateManhattanDistance)
+    printfn "First doubly visied location: %A" result.FirstDoubleLocation
+    printfn "Distance to first doubly visited location: %A" (result.FirstDoubleLocation.Value |> calculateManhattanDistance)
     0
