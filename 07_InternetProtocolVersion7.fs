064b6815c82992b8b83efb897885d0bc051d5044
Chris Jansson <chris.janssom@gmail.com>
2016-12-08 21:57:47 +0100

    updated parsing code a bit

--- 07_InternetProtocolVersion7.fs
+++ 07_InternetProtocolVersion7.fs
@@ -1,10 +1,19 @@
 module InternetProtocolVersion7
 
-let private addressRegex = System.Text.RegularExpressions.Regex("^(.*?)\[(.*?)\](.*?)$")
+let private addressRegex = System.Text.RegularExpressions.Regex("(([^\[\]]\w+[^\[\]])|([\[\]]\w+[\[\]]))")
 
 let parseAddress addressString =
-    let m = addressRegex.Match(addressString)
-    (m.Groups.[1].Value, m.Groups.[2].Value, m.Groups.[3].Value)
+    let groupCollectionToSequence (mc:System.Text.RegularExpressions.MatchCollection) = seq {
+        for i = 0 to (mc.Count - 1) do
+            let gc = mc.[i].Groups
+            for j = 0 to (gc.Count - 1) do
+                let value = gc.[j].Value
+                if value <> "" then
+                    yield value
+    }
+
+    let matches = addressRegex.Matches(addressString)
+    matches |> groupCollectionToSequence
 
 let rec hasAbba (addressPart:string) =
     let isAbba (s:string) = s.[0] = s.[3] && s.[1] = s.[2] && s.[0] <> s.[1]
@@ -21,10 +30,16 @@ let private isValidAddress (p0, p1, p2) =
     | _ -> false
 
 let main input =
-    [
-        input 
+    input 
             |> System.IO.File.ReadAllLines
-            |> Array.map (parseAddress >> isValidAddress) 
-            |> Array.filter id
-            |> (fun a -> sprintf "%A" a.Length)     
-    ]
+            |> Array.map parseAddress
+            |> Array.map (sprintf "%A")
+            |> Array.toList
+    
+    // [
+        
+    //         //|> Array.map (sprintf "%A")
+    //         //|> Array.map (parseAddress >> isValidAddress) 
+    //         // |> Array.filter id
+    //         // |> (fun a -> sprintf "%A" a.Length)     
+    // ]
