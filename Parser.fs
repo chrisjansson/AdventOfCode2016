e6ddebae8fa80be56e165925c50d533aadfb6b7e
Chris Jansson <chris.janssom@gmail.com>
2016-12-15 17:20:43 +0100

    Happy path parser composition

--- Parser.fs
+++ Parser.fs
@@ -31,3 +31,19 @@ let ``Fails parsing empty string``() =
     let result = pChar 'x' ""
     let expected = Error ("")
     Assert.Equal(expected, result)
+
+module Operators =
+
+let compose p0 p1 s =
+    let r, rem = match p0 s with | Ok (r, rem) -> (r, rem)
+    let r2,rem2 = match p1 rem with | Ok (r, rem) -> (r, rem)
+    Ok ((r, r2), rem2)
+
+[<Fact>]
+let ``Combines the result of two parsers``() =
+    let p0 = pChar 'a'
+    let p1 = pChar 'b'
+    let parser = compose p0 p1
+    let result = parser "abc"
+    let expected = Ok (('a', 'b'), "c")
+    Assert.Equal(expected, result)
